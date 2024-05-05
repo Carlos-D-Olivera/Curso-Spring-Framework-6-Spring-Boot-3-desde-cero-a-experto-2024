df9ec0803f23d67f8373e2ffb825400670b3d703
CarlosDOG <carlosdavidoliverag@gmail.com>
2024-05-05 12:18:11 -0500

    El objeto model: pasandole un Objeto user a la vista

--- springboot-web/src/main/java/com/carlos/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/carlos/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -3,31 +3,29 @@ package com.carlos.curso.springboot.webapp.springbootweb.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlos.curso.springboot.webapp.springbootweb.models.User;
 
 
 //   RestController: Es una combinacion entre @Controller y @RestController
 //    ¬ Se usa para indicar que todos los valores devueltos por lo metodos seran enviados directamente a la respuesta HTTP en vez de una vista renderizada
-//@RestController
-@Controller
-@RequestMapping("/api")
+@RestController
+@RequestMapping("/api")// ruta de primer nivel o base
 public class UserRestController {
 
     //Usando un Map para pasar los datos
-    //@GetMapping("/details") es la abrevacion para @RequestMapping(path="/details", method=RequestMethod.GET)
-    @RequestMapping(path="/details", method=RequestMethod.GET) //Es lo mismo que @GetMapping, @RequestMapping por defecto es de tipo GET pero se le puede especificar el tipo con el atributo method method=RequestMethod.GET
-    @ResponseBody //Si se esta usando Controller y no restController toca poner esta anotacion para indicar que la respuesta se manda directamente
+    @GetMapping("/details")// es la abrevacion para @RequestMapping(path="/details", method=RequestMethod.GET)
     public Map<String, Object> details(){
+
+        User user = new User("Carlos", "Olivera", 19);
 
         Map<String, Object> body = new HashMap<>();
 
         body.put("title","Hola mundo en Spring boot con Map");
-        body.put("name", "Carlos");
-        body.put("lastName","Olivera");
-        body.put("age", "19");
+        body.put("user", user);
 
         return body; //se devuelve directamente el valor del objeto
     }
---
+++ springboot-web/src/main/java/com/carlos/curso/springboot/webapp/springbootweb/models/User.java
@@ -0,0 +1,39 @@
+package com.carlos.curso.springboot.webapp.springbootweb.models;
+
+public class User {
+    private String name;
+    private String lastname;
+    private int age;
+
+
+    public User(String name, String lastname, int age) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName(){
+        return this.name;    
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+}
