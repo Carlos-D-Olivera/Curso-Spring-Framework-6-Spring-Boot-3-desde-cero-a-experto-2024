7e0b79b09c2d22af83a4f218c2813618ed137f5c
Carlos <carlosdavidoliverag@gmail.com>
2024-05-14 13:11:59 -0500

    @PostMapping - @RequestBody: obtener datos con el metodo POST

--- springboot-web/src/main/java/com/carlos/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/carlos/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -1,10 +1,9 @@
 package com.carlos.curso.springboot.webapp.springbootweb.controllers;
 
+import com.carlos.curso.springboot.webapp.springbootweb.models.User;
 import com.carlos.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,5 +27,37 @@ public class PathVariableController {
         json.put("producto", product);
         json.put("id",id);
         return json;
+    }
+
+    //Por lo general el post no lleva ruta sino la ruta base
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        //aqui se guardaria el usuario
+        user.setName(user.getName().concat(" guardado").toUpperCase());
+        return user;
+    }
+    
+    
+    //Metodo para crear un usuario mediante una ruta variable
+    @PostMapping("/create/{name}/{lastname}")
+    public User createByPath(@PathVariable String name, @PathVariable String lastname, @RequestParam(name="correo", defaultValue = "SIN CORREO") String email, HttpServletRequest request){
+        
+        //Creamos el usuario
+        User u = new User();
+        u.setName(name);
+        u.setLastName(lastname);
+        u.setEmail(email);
+
+        int edad = 0;
+        try{
+            edad = Integer.parseInt(request.getParameter("edad"));
+        }catch(NumberFormatException e){
+
+        }
+        u.setAge(edad);
+
+        //se retorna el usuario creado
+        return u;
+        
     }
 }
