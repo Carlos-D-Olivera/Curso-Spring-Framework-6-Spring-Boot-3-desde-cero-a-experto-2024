7aaaf4d6da5eab9a8dcb3ccffdb4c61683d0cd9b
Carlos <carlosdavidoliverag@gmail.com>
2024-06-10 08:30:25 -0500

    Clase model y repository

---
+++ SpringBoot-di/src/main/java/com/cdog/springboot/di/app/SpringBoot_di/models/Product.java
@@ -0,0 +1,41 @@
+package com.cdog.springboot.di.app.SpringBoot_di.models;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
---
+++ SpringBoot-di/src/main/java/com/cdog/springboot/di/app/SpringBoot_di/repositories/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.cdog.springboot.di.app.SpringBoot_di.repositories;
+
+import com.cdog.springboot.di.app.SpringBoot_di.models.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L, "Hot dog", 5000L),
+            new Product(2L, "CPU Intel Core i9",850000L),
+            new Product(3L, "Teclado Razer mini 60%", 180000L),
+            new Product(4L, "Motherboard Gigabyte",490000L)
+        );
+    }
+
+    public List<Product> findAll(){
+        return this.data;
+    }
+}
