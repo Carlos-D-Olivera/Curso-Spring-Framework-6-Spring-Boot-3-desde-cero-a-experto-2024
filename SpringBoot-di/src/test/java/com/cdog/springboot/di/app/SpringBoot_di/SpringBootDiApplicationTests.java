3309a7b3d0fb3c1cfd22aec19cb54111614f3537
Carlos <carlosdavidoliverag@gmail.com>
2024-06-09 21:54:00 -0500

    44 - Creando un nuevo proyecto

---
+++ SpringBoot-di/src/test/java/com/cdog/springboot/di/app/SpringBoot_di/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdog.springboot.di.app.SpringBoot_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
