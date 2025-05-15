aae76b6e7f354210d3b2ac626a95ff32fdf13a99
CodgPro <carlosdavidoliverag@gmail.com>
2025-05-14 22:41:48 -0500

    75. Contextos (Scope) de los componentes

--- springboot-difactura/src/main/java/org/colivera/springbootdifactura2/models/Client.java
+++ springboot-difactura/src/main/java/org/colivera/springbootdifactura2/models/Client.java
@@ -1,9 +1,15 @@
 package org.colivera.springbootdifactura2.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component //significa que sera gestionado por el contenedor de spring y podra ser inyectado
+//@SessionScope //El objeto cliente sera unico por cada sesion
+@RequestScope //El cliente se creara en cada peticion
+@JsonIgnoreProperties({"targetSource", "advisors"})
 public class Client {
 
     @Value("${client.name}")
--- springboot-difactura/src/main/java/org/colivera/springbootdifactura2/models/Invoice.java
+++ springboot-difactura/src/main/java/org/colivera/springbootdifactura2/models/Invoice.java
@@ -1,15 +1,19 @@
 package org.colivera.springbootdifactura2.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
 
 @Component
+@RequestScope() //Esta objeto se crea en cada peticion
+@JsonIgnoreProperties({"targetSource", "advisors"})
 public class Invoice {
 
     @Autowired
