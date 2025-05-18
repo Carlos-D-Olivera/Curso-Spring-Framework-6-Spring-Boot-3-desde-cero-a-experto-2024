86048789958b803b276f283cc8ce293f66eee236
CodgPro <carlosdavidoliverag@gmail.com>
2025-05-18 13:41:56 -0500

    78. Creando nueva aplicación y controlador (SpringbootError)

---
+++ springboot-error/src/main/java/org/colivera/curso/springboot/error/springbooterror/SpringbootErrorApplication.java
@@ -0,0 +1,13 @@
+package org.colivera.curso.springboot.error.springbooterror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootErrorApplication.class, args);
+    }
+
+}
---
+++ springboot-error/src/test/java/org/colivera/curso/springboot/error/springbooterror/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.colivera.curso.springboot.error.springbooterror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
