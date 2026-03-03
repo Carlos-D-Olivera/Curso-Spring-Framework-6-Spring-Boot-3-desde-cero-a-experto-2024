7fc4d698256eb8d6f0c2cd470c5810d5ba3a4453
Carlos Olivera <carlosdavidoliverag@gmail.com>
2026-03-02 21:36:06 -0500

    205-213. Se implementan filtros de Generacion y Validacion de JWT

--- springboot-crud/src/main/java/com/carlos/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
+++ springboot-crud/src/main/java/com/carlos/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.carlos.curso.springboot.app.springbootcrud.security;
 
+import com.carlos.curso.springboot.app.springbootcrud.security.filter.JwtAuthenticationFilter;
+import com.carlos.curso.springboot.app.springbootcrud.security.filter.JwtValidationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -22,6 +35,8 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests((authz)-> authz.requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                                                          .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                                                          .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtValidationFilter(authenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(management ->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
--- springboot-crud/src/main/java/com/carlos/curso/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
+++ springboot-crud/src/main/java/com/carlos/curso/springboot/app/springbootcrud/security/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,108 @@
 package com.carlos.curso.springboot.app.springbootcrud.security.filter;
 
-public class JwtAuthenticationFilter {
+import com.carlos.curso.springboot.app.springbootcrud.entities.User;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.carlos.curso.springboot.app.springbootcrud.security.TokenJwtConfig.*;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//Esta clase se encarga de autenticar el usuario y generar el token de acceso
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        User user;
+        String username;
+        String password;
+
+        try{
+            user = new ObjectMapper().readValue(request.getInputStream(),User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (DatabindException e) {
+            throw new RuntimeException(e);
+        } catch (StreamReadException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        String username = user.getUsername();
+
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+        Claims claims = Jwts.claims()
+                .add("authorities", new ObjectMapper().writeValueAsString(roles))
+                .add("username", username)
+                .build();
+
+        String token = Jwts.builder()
+                .subject(username)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis()+3600000))//Tiempo que va a durar el token
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token",token);
+        body.put("username",username);
+        body.put("message",String.format("Hola %s has iniciado sesion con exito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        Map<String, String> body = new HashMap<>();
+        body.put("message", "Error en la autenticacion: username o password incorrectos!");
+        body.put("error", failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(CONTENT_TYPE);
+    }
 }
